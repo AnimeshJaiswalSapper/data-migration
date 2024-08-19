c28fffbe252f501530c63e04e19e31d17dfb40d2
AnimeshJaiswalSapper <165006989+AnimeshJaiswalSapper@users.noreply.github.com>
2024-08-19 17:33:23 +0530

    created java service for migration

---
+++ src/main/java/ai/sapper/migration/DataMigration/model/DataMigration.java
@@ -0,0 +1,11 @@
+package ai.sapper.migration.DataMigration.model;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("dataMigration")
+public class DataMigration {
+
+}
---
+++ src/test/java/ai/sapper/migration/DataMigration/DataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.sapper.migration.DataMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
