6baf7e21646da4424844efe4f437d7221b24d95f
TrilokiSapper <Triloki.nath@sapper.ai>
2024-08-19 17:44:57 +0530

    initial commit

--- src/main/java/ai/sapper/migration/DataMigration/DataMigrationApplication.java
+++ src/main/java/ai/sapper/migration/DataMigration/DataMigrationApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataMigrationApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(DataMigrationApplication.class, args);
 	}
